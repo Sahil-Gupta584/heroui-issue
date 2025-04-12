59dc633dfd04ed273ef0ea2c0713822cc8b80727
Sahil Gupta <guptas3067@gmail.com>
2025-04-12 21:11:34 +0530

    some changes

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+import { heroui } from "@heroui/react";
+
+/**@type {import("tailwindcss").Config} */
+const config = {
+  content: [
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",
+
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+      },
+    },
+  },
+  plugins: [heroui()],
+};
+
+export default config;
